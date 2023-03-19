4ef17ad926f75cc13f36f18dfc9a42e45b4daa4c
eliasinacio <elias.chavier@jbq.global>
2023-03-19 14:45:26 -0300

    paddles moves and ball animation

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
-const canvasContainer = document.querySelector('#ping-pong')
+const canvasContainer = document.body
 
 const gapX = 10
+const mouse = { x: 0, y: 0 }
 
 const field = {
     width: canvasContainer.offsetWidth,
-    height: canvasContainer.offsetHeight * 2 / 3,
+    height: canvasContainer.offsetHeight,
     draw: function () {
         context.fillStyle = '#286047'
         context.fillRect(0, 0, this.width, this.height)
@@ -25,22 +26,35 @@ const line = {
 }
 
 const leftPaddle = {
-    x: field.width - line.width - gapX,
-    y: 0,
-    width: line.width,
-    height: 140,
-    draw: function () {
-        context.fillRect(this.x, this.y, this.width, this.height)
-    }
-}
-
-const rightPaddle = {
     x: gapX,
     y: 0,
     width: line.width,
     height: 140,
+
+    _move: function () {
+        const pos = mouse.y - this.height / 2
+        this.y = pos > 0 ? pos : 0
+    },
+
     draw: function () {
         context.fillRect(this.x, this.y, this.width, this.height)
+        this._move()
+    }
+}
+
+const rightPaddle = {
+    x: field.width - line.width - gapX,
+    y: 0,
+    width: line.width,
+    height: 140,
+
+    _move: function () {
+        this.y = ball.y - this.height / 2
+    },
+
+    draw: function () {
+        context.fillRect(this.x, this.y, this.width, this.height)
+        this._move()
     }
 }
 
@@ -49,18 +63,47 @@ const ball = {
     y: 300,
     radius: 15,
     speed: 5,
+    dirX: 1,
+    dirY: 1,
+
+    _positions: function () {
+        if (this.y > field.height) {
+            this._reverseY()
+        }
+
+        if (this.y == 0) {
+            this._reverseY()
+        }
+
+        if (this.x === rightPaddle.x) {
+            this._reverseX()
+        }
+        
+        // if (this.y > field.height) {
+        //     this._reverseY
+        // }
+    },
+
+    _reverseX: function () {
+        this.dirX *= -1
+    },
+
+    _reverseY: function () {
+        this.dirY *= -1
+    },
 
     _move: function () {
-        this.y += 1 * this.speed
-        this.x += 1 * this.speed
+        this.x += this.speed * this.dirX
+        this.y += this.speed * this.dirY
     },
-    
+
     draw: function () {
         context.beginPath()
         context.fillStyle = '#fff'
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
         context.fill()
 
+        this._positions()
         this._move()
     }
 }
@@ -92,7 +135,7 @@ function draw() {
     line.draw()
     leftPaddle.draw()
     rightPaddle.draw()
-    
+
     ball.draw()
     score.draw('3', '3')
 }
@@ -103,17 +146,42 @@ window.animateFrame = (function () {
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame || 
+        window.msRequestAnimationFrame ||
         function (callback) {
             return window.setTimeout(callback, 1000 / 60)
         }
     )
 })()
 
-function main () {
+function main() {
     animateFrame(main)
     draw()
 }
 
 setup()
 main()
+
+canvas.addEventListener('mousemove', function (e) {
+    mouse.x = e.pageX
+    mouse.y = e.pageY
+})
+
+// 2 players
+
+// document.addEventListener('keypress', function (e) {
+//     console.log(e)
+
+//     if (e.key == 33) {
+//         mouse.y += 5
+//     }
+// })
+
+// document.onkeydown = (e) => {
+//     e = e || window.event;
+
+//     if (e.key === 'ArrowUp') {
+//         rightPaddle.y -= 20
+//     } else if (e.key === 'ArrowDown') {
+//         rightPaddle.y += 20
+//     }
+// }
